8268179f584594b71a0776b0cd816b801ac77849
zhongyuhong <4a22c902@stust.edu.tw>
2017-11-30 09:49:33 +0000

    first commit

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install
+
+sudo apt-get install python3-pip
+sudo apt-get install git
+sudo apt-get install vim
+sudo apt-get install samba
+sudo apt-get install samba-common-bin
+
+sudo apt-get install ssh vim gcc g++ samba make less git
+
+sudo aptitude install openssh-server
+sudo apt-get install openssh-server
+sudo /etc/init.d/ssh restart
+
+
---
+++ py.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sudo apt-get install python3-pip
+sudo apt-get install mosquitto mosquitto-clients
+sudo pip3 install paho-mqtt
+
+sudo pip3 install flask
+
+sudo pip3 install requests
+sudo pip3 install BeautifulSoup4
+
+#sudo apt-get install bluetooth bluez blueman
+
