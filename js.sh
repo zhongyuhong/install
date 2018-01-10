4c485d023ca41806b80c28ffea6b8180efa61f97
zhongyuhong <4a22c902@stust.edu.tw>
2018-01-10 05:21:22 +0000

    init commit

---
+++ js.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+sudo apt-get install nodejs
+sudo apt-get install npm
+
+sudo git clone https://github.com/nodejs/node.git
+
+sudo chmod -R 755 node
+
+sudo npm install socket.io --save
+sudo npm install request cheerio
+sudo npm install express utility --save
+sudo npm install --save PACKAGE_NAME
+sudo npm install mqtt --save
+sudo npm install pm2 -g
+sudo npm install gpio
+sudo npm install i2c
+
+
+sudo apt-get upgrade
--- py.sh
+++ py.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
+sudo apt-get install python3
 sudo apt-get install python3-pip
-sudo apt-get install mosquitto mosquitto-clients
+#sudo apt-get install mosquitto mosquitto-clients
 sudo pip3 install paho-mqtt
-
 sudo pip3 install flask
 
 sudo pip3 install requests
 sudo pip3 install BeautifulSoup4
+sudo pip3 install RPi.GPIO
+sudo modprobe i2c-dev
+sudo apt-get install i2c-tools
+
 
 #sudo apt-get install bluetooth bluez blueman
+sudo apt-get upgrade
 
