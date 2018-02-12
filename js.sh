c07e184144cd624ad2980ee9d2fd673132925fb6
zhongyuhong <4a22c902@stust.edu.tw>
2018-02-12 14:17:22 +0000

    install

--- js.sh
+++ js.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-sudo apt-get install nodejs
-sudo apt-get install npm
-
 sudo git clone https://github.com/nodejs/node.git
 
 sudo chmod -R 755 node
+
+cd node
+sudo ./configure
+sudo make
+sudo make install
 
 sudo npm install socket.io --save
 sudo npm install request cheerio
@@ -15,10 +17,13 @@ sudo npm install mqtt --save
 sudo npm install pm2 -g
 sudo npm install gpio
 sudo npm install i2c
+sudo npm install -g onoff
 
+sudo apt-get install build-essential
+sudo npm install -g blynk-library
+sudo apt-get install build-essential
 
 sudo apt-get upgrade
-
 
 npm --version
 
